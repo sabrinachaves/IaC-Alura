037f14e2d04092d9ab8256e96ace72c1c3e44858
Sabrina Chaves <sabrinach08@gmail.com>
2023-06-12 09:15:36 -0300

    chore: change instance name

--- main.tf
+++ main.tf
@@ -17,8 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
   key_name = "iac-key-alura"
+#   user_data = <<-EOF
+#                  #!/bin/bash
+#                  cd /home/ubuntu    
+#                  echo "<h1> Hello World!</h1>" > index.html
+#                  nohup busybox httpd -f -p 8080 &
+#                  EOF
 
   tags = {
-    Name = "FirstInstance"
+    Name = "Terraform Ansible Python"
   }
 }
